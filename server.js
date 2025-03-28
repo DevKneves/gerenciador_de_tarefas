d24125306403699d49d9b5e306881bd8334f39f6
Devkneves <kn180732@gmail.com>
2025-03-28 11:08:46 -0300

    atualizando package json e backend para hospedar o servidor na web

--- server.js
+++ server.js
@@ -189,6 +189,5 @@ app.delete('/tasks/:id', verifyToken, async (req, res) => {
 });
 
 // Iniciar o servidor
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
